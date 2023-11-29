e97ce8484dc7d523f3c04746e35c792a662a534f
unknown <20220876@duksung.ac.kr>
2023-11-29 13:00:25 +0900

    base and signup modified

--- dp/urls.py
+++ dp/urls.py
@@ -6,8 +6,9 @@ from .views import logout
 
 
 urlpatterns = [
+    path('logout/', views.logout, name='logout'),
     path('store/', views.store),
-    path('', views.main),
+    path('', views.main, name='main'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('signup2/', SignUpView2.as_view(), name='signup2'),
     path('signin/', SignInView.as_view(), name='signin'),
